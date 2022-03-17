f2eb2c42f7a3d611cb8a7bf0cc94142d278aa09a
Kacper Razniak <X00174865@mytudublin.ie>
2022-03-17 17:39:44 +0000

    Unable to accept friend request. Seems like its not finding the request id

--- friend/views.py
+++ friend/views.py
@@ -1,38 +1,85 @@
-from email.mime import application
-import imp
+from django.shortcuts import render, redirect
+
 from django.http import HttpResponse
 import json
 
 from account.models import Account
 from friend.models import FriendRequest
 
-def send_friend_request(request, *args, **kwargs):
-    user = request.user
-    payload = {}
-    if request.method == "POST" and user.is_authenticated:
-        user_id = request.POST.get("receiver_user_id")
-        if user_id:
-            receiver = Account.objects.get(pk=user_id)
-            try:
-                friend_requests = FriendRequest.objects.filter(sender = user, receiver = receiver)
-                try:
-                    for request in friend_requests:
-                        if request.is_active:
-                            raise Exception("You already sent them a frind request.")
-                    friend_request = FriendRequest(sender = user , receiver = receiver)
-                    friend_request.save()
-                    payload['response'] = "Friend request sent."
-                except Exception as e:
-                    payload['response'] = str(e)
-            except FriendRequest.DoesNotExist:
-                friend_request = FriendRequest(sender = user, receiver = receiver)
-                friend_request.save()
-                payload["response"] = "Friend request sent."
 
-            if payload["response"] == None:
-                payload["response"] = "Something went wrong."       
-        else:
-            payload["response"] = "Unable to send a frind request."
-    else:
-        payload["response"] = "You must be authenticated to send a friend request."
-    return HttpResponse(json.dumps(payload), content_type = "application/json")
+def friend_requests(request, *args, **kwargs):
+	context = {}
+	user = request.user
+	if user.is_authenticated:
+		user_id = kwargs.get("user_id")
+		account = Account.objects.get(pk=user_id)
+		if account == user:
+			friend_requests = FriendRequest.objects.filter(receiver=account, is_active=True)
+			context['friend_requests'] = friend_requests
+		else:
+			return HttpResponse("You can't view another users friend requets.")
+	else:
+		redirect("login")
+	return render(request, "friend/friend_requests.html", context)
+
+
+def send_friend_request(request, *args, **kwargs):
+	user = request.user
+	payload = {}
+	if request.method == "POST" and user.is_authenticated:
+		user_id = request.POST.get("receiver_user_id")
+		if user_id:
+			receiver = Account.objects.get(pk=user_id)
+			try:
+				# Get any friend requests (active and not-active)
+				friend_requests = FriendRequest.objects.filter(sender=user, receiver=receiver)
+				# find if any of them are active (pending)
+				try:
+					for request in friend_requests:
+						if request.is_active:
+							raise Exception("You already sent them a friend request.")
+					# If none are active create a new friend request
+					friend_request = FriendRequest(sender=user, receiver=receiver)
+					friend_request.save()
+					payload['response'] = "Friend request sent."
+				except Exception as e:
+					payload['response'] = str(e)
+			except FriendRequest.DoesNotExist:
+				# There are no friend requests so create one.
+				friend_request = FriendRequest(sender=user, receiver=receiver)
+				friend_request.save()
+				payload['response'] = "Friend request sent."
+
+			if payload['response'] == None:
+				payload['response'] = "Something went wrong."
+		else:
+			payload['response'] = "Unable to sent a friend request."
+	else:
+		payload['response'] = "You must be authenticated to send a friend request."
+	return HttpResponse(json.dumps(payload), content_type="application/json")
+
+
+def accept_friend_request(request, *args, **kwargs):
+	user = request.user
+	payload = {}
+	if request.method == "GET" and user.is_authenticated:
+		friend_request_id = kwargs.get("friend_request_id")
+		if friend_request_id:
+			friend_request = FriendRequest.objects.get(pk=friend_request_id)
+			# confirm that is the correct request
+			if friend_request.receiver == user:
+				if friend_request:
+					# found the request. Now accept it
+					updated_notification = friend_request.accept()
+					payload['response'] = "Friend request accepted."
+
+				else:
+					payload['response'] = "Something went wrong."
+			else:
+				payload['response'] = "That is not your request to accept."
+		else:
+			payload['response'] = "Unable to accept that friend request."
+	else:
+		# should never happen
+		payload['response'] = "You must be authenticated to accept a friend request."
+	return HttpResponse(json.dumps(payload), content_type="application/json")
